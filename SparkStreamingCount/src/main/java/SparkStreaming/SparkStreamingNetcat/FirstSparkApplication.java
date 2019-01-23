2234ff6cc4387995f7a23091cd1c3698868bfbd9
Kumar Kunal <kumar.kunal.bd@gmail.com>
2019-01-24 01:42:37 +0530

    adding codes

---
+++ SparkStreamingCount/src/main/java/SparkStreaming/SparkStreamingNetcat/FirstSparkApplication.java
@@ -0,0 +1,22 @@
+package SparkStreaming.SparkStreamingNetcat;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.apache.log4j.*;
+
+public class FirstSparkApplication {
+
+	public static void main(String[] args) throws InterruptedException {
+		SparkConf conf = new SparkConf().setMaster("local[*]").setAppName("FirstSparkApplication");
+		JavaStreamingContext jssc = new JavaStreamingContext(conf, Durations.seconds(5));
+		Logger.getRootLogger().setLevel(Level.ERROR);
+		JavaReceiverInputDStream<String> lines = jssc.socketTextStream("localhost", 9999);
+		lines.print();
+		jssc.start();
+		jssc.awaitTermination();
+		jssc.close();
+	}
+
+}
