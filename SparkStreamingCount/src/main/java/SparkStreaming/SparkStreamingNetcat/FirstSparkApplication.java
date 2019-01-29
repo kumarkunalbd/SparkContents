8c38da0ab9147e88c21ca9c92bb85c598489a241
Kumar Kunal <kumar.kunal.bd@gmail.com>
2019-01-30 01:28:37 +0530

    Changes related to Window operation

--- SparkStreamingCount/src/main/java/SparkStreaming/SparkStreamingNetcat/FirstSparkApplication.java
+++ SparkStreamingCount/src/main/java/SparkStreaming/SparkStreamingNetcat/FirstSparkApplication.java
@@ -2,6 +2,7 @@ package SparkStreaming.SparkStreamingNetcat;
 
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.Optional;
+import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.streaming.Duration;
 import org.apache.spark.streaming.Durations;
 import org.apache.spark.streaming.api.java.JavaDStream;
@@ -20,10 +21,10 @@ public class FirstSparkApplication {
 
 	public static void main(String[] args) throws InterruptedException {
 		SparkConf conf = new SparkConf().setMaster("local[*]").setAppName("FirstSparkApplication");
-		JavaStreamingContext jssc = new JavaStreamingContext(conf, Durations.seconds(1));
-		//jssc.checkpoint("checkpoint_dir");
+		JavaStreamingContext jssc = new JavaStreamingContext(conf, Durations.seconds(2));
+		jssc.checkpoint("checkpoint_dir");
 		Logger.getRootLogger().setLevel(Level.ERROR);
-		JavaReceiverInputDStream<String> lines = jssc.socketTextStream("localhost", 9999);
+		//JavaReceiverInputDStream<String> lines = jssc.socketTextStream("localhost", 9999);
 		/*JavaDStream<String> words = lines.flatMap(x->Arrays.asList(x.split(" ")).iterator());
 		JavaPairDStream<String, Integer> pairs = words.mapToPair(s -> new Tuple2<>(s, 1));
 		JavaPairDStream<String, Integer> updatedPairs = pairs.updateStateByKey((values,currentState)->{
@@ -35,7 +36,26 @@ public class FirstSparkApplication {
 			
 		});
 		updatedPairs.print();*/
-		lines.window(Durations.seconds(6),Durations.seconds(4)).print();
+		//lines.window(Durations.seconds(6),Durations.seconds(4)).print();
+		/* Implementing window operations*/
+		Function2<String,String, String> reduceFunct = new Function2<String, String, String>() {
+			@Override 
+			public String call(String str1, String str2) throws Exception {
+				return "Window is filled";
+			}
+		};
+		
+		Function2<String,String, String> invReduceFunct = new Function2<String, String, String>() {
+			@Override 
+			public String call(String str1, String str2) throws Exception {
+				return "Window is empty";
+			}
+		};
+		JavaReceiverInputDStream<String> lines = jssc.socketTextStream("localhost", 9999);
+		//lines.flatMap(line -> Arrays.asList(line.split(​" "​)).iterator()).reduceByWindow(reduceFunc, invReduceFunc, Durations.seconds(​10​), Durations.seconds(​6​)).print();
+		lines.flatMap(line -> Arrays.asList(line.split("")).iterator())
+			.reduceByWindow(reduceFunct, invReduceFunct, Durations.seconds(10), Durations.seconds(6))
+			.print(); 
 		jssc.start();
 		jssc.awaitTermination();
 		jssc.close();
