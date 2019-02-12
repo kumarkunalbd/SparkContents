8183b9bc4604c683d0fb701f8225c09028bbe1e9
Kumar Kunal <kumarkunal@apples-MacBook-Pro.local>
2019-02-13 00:34:06 +0530

    Spark steramig is ruig o live data

---
+++ NSYEAnalysis/src/main/java/Analysis/Analyser.java
@@ -0,0 +1,72 @@
+package Analysis;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.Optional;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.streaming.Duration;
+import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaPairDStream;
+import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+
+import scala.Tuple2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.apache.log4j.*;
+
+public class Analyser {
+
+	public static void main(String[] args) throws InterruptedException{
+		// TODO Auto-generated method stub
+		SparkConf conf = new SparkConf().setMaster("local[*]").setAppName("StockAnalyser");
+		JavaStreamingContext jssc = new JavaStreamingContext(conf, Durations.seconds(60));
+		jssc.checkpoint("checkpoint_dir");
+		Logger.getRootLogger().setLevel(Level.ERROR);
+		//JavaReceiverInputDStream<String> lines = jssc.socketTextStream("localhost", 9999);
+		/*JavaDStream<String> words = lines.flatMap(x->Arrays.asList(x.split(" ")).iterator());
+		JavaPairDStream<String, Integer> pairs = words.mapToPair(s -> new Tuple2<>(s, 1));
+		JavaPairDStream<String, Integer> updatedPairs = pairs.updateStateByKey((values,currentState)->{
+			int sum = currentState.or(0);
+			for (int i: values) {
+				sum += i;
+			}
+			return Optional.of(sum);
+			
+		});
+		updatedPairs.print();*/
+		//lines.window(Durations.seconds(6),Durations.seconds(4)).print();
+		/* Implementing window operations*/
+		Function2<Integer,Integer, Integer> reduceFunct = new Function2<Integer, Integer, Integer>() {
+			public Integer call(Integer valuePresent, Integer valueIncoming) throws Exception {
+				System.out.println("Summary function is running");
+				System.out.println(valuePresent+"+"+valueIncoming);
+				return valuePresent+valueIncoming;
+				
+			}
+		};
+		
+		Function2<Integer,Integer, Integer> invReduceFunct = new Function2<Integer,Integer, Integer>() {
+			public Integer call(Integer valuePresent, Integer valueOutgoing) throws Exception {
+				System.out.println("Inverse function is running");
+				System.out.println(valuePresent+"-"+valueOutgoing);
+				return valuePresent-valueOutgoing;
+			}
+		};
+		JavaDStream<String> lines = jssc.textFileStream("/Users/kumarkunal/Upgrad_materials/Course5-Mod7-SparkStream/NSYE_Data-12Feb2");
+		lines.print();
+		//lines.flatMap(line -> Arrays.asList(line.split(​" "​)).iterator()).reduceByWindow(reduceFunc, invReduceFunc, Durations.seconds(​10​), Durations.seconds(​6​)).print();
+		/*lines.flatMap(line -> Arrays.asList(line.split(" ")).iterator())
+					.mapToPair(line -> new Tuple2<>(line, 1))
+					.reduceByKeyAndWindow(reduceFunct, invReduceFunct, Durations.seconds(10), Durations.seconds(6))
+					.print();*/
+		
+		jssc.start();
+		jssc.awaitTermination();
+		jssc.close();
+
+	}
+
+}
